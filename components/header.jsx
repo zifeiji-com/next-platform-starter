ba93765e8da26ec31962341ef4d28c782aebd208
纸飞机中文版 <cblockm3game@protonmail.com>
2025-03-19 15:56:05 +0800

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -15,9 +15,9 @@ const navItems = [
 export function Header() {
     return (
         <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
-            <Link href="/">
-                <img src="https://www.zifeiji.com/image/icon.png" alt="zifeiji logo" />
-            </Link>
+            
+                <img src="https://www.zifeiji.com/image/icon.png" alt="zifeiji logo" >
+            
             {!!navItems?.length && (
                 <ul className="flex flex-wrap gap-x-4 gap-y-1">
                     {navItems.map((item, index) => (
